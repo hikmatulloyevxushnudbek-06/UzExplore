57e5a551ed67b0dc7ff429db0fdae61fa15508ad
Xushnudbek Hikmatulloyev <hikmatulloyevxushnudbek06@gmail.com>
2026-03-12 11:16:33 +0500

    Start

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Destinations from './pages/Destinations'
+import Gallery from './pages/Gallery'
+import Blog from './pages/Blog'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/destinations" element={<Destinations />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
